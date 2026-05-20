472813dea97dfd4082599dcc1e2274ac5a006d29
oe1wkl <willi@kraml.cc>
2026-05-20 17:30:19 +0200

    Update morsedefs.h
    Set to 8.1 beta

--- Software/src/Version 6 and newer/morsedefs.h
+++ Software/src/Version 6 and newer/morsedefs.h
@@ -41,14 +41,14 @@ const char* const PROJECTNAME = "Morserino-32";
 const char* const COPYRIGHT = "\xc2\xa9 2018-2026";  // © in UTF-8
 
 #define VERSION_MAJOR 8
-#define VERSION_MINOR 0
+#define VERSION_MINOR 1
 #define VERSION_PATCH 0
 
-#define BETA false
+#define BETA true
 #define COMPILEDATE __DATE__
 
 
-#define IGNORE_SERIALOUT false
+#define IGNORE_SERIALOUT true
 
 // if IGNORE_SERIALOUT is true, alle DEBUG messages are on serial out, even when Serial Out is active outputting characters from Keyer, Decoder etc
 
